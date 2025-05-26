f70dd37806a882a6d70a43d19a6c0a72fd0c7dc1
CicadaMusic <team@cicadamusic.net>
2025-05-27 00:51:21 +0300

    change docker compose instalation method

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -33,16 +33,18 @@ resource "aws_instance" "app_server" {
 
   user_data = <<-EOF
               #!/bin/bash
+              # Update system
               apt-get update
+              
+              # Install Docker
               apt-get install -y docker.io
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ubuntu
               
               # Install Docker Compose
-              curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              chmod +x /usr/local/bin/docker-compose
-              ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+              apt-get install -y docker-compose-plugin
+              ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
               
               # Verify installations
               docker --version
